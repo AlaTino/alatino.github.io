820c9b3c601eaab296e4753ed66ea72467089a9d
Tino Alakari <150487495+AlaTino@users.noreply.github.com>
2024-12-05 03:10:09 -0700

    Add files via upload

--- DesertGarden/formValidation.js
+++ DesertGarden/formValidation.js
@@ -2,11 +2,17 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     const name = document.getElementById('name').value.trim();
     const email = document.getElementById('email').value.trim();
     const comments = document.getElementById('comments').value.trim();
-  
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    
     if (!name || !email || !comments) {
       alert('Please fill out all fields.');
       event.preventDefault();
-    } else {
+    } 
+    else if (!emailPattern.test(email)) {
+      alert('Please enter a valid email address.')
+      event.preventDefault();
+    }
+    else {
       alert('Form submitted successfully!');
     }
   });
